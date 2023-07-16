4cffaf42b4e62bc76c299132b6a783f041c04fe4
lucas-faget <lucas.faget@laposte.net>
2023-07-16 16:24:19 +0200

    Set up A4 page to download

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,43 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+    import './app.css';
+
+    import html2pdf from 'html2pdf.js';
+
+    function generatePDF() {
+        const content: HTMLElement = document.querySelector('#cv') as HTMLElement;
+        const opt = {
+            margin:       0,
+            filename:     'CV_FAGET_Lucas.pdf',
+            image:        { type: 'jpeg', quality: 0.98 },
+            html2canvas:  { scale: 2 },
+            jsPDF:        { unit: 'mm', format: 'a4', orientation: 'portrait' }
+        };
+  
+        html2pdf().set(opt).from(content).save();
+    }
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+    <button on:click={generatePDF}>Télécharger en PDF</button>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+    <div id="cv">
+        
+    </div>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+    main {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 20px;
+        margin-block: 50px;
+    }
+
+    #cv {
+        background-color: red;
+        width: 210mm;
+        height: 297mm;
+    }
 </style>
