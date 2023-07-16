4343fd19c5110e12d39abb448aa0b6228bb6213a
lucas-faget <lucas.faget@laposte.net>
2023-07-16 19:45:03 +0200

    CV skeleton

--- src/App.svelte
+++ src/App.svelte
@@ -21,23 +21,80 @@
     <button on:click={generatePDF}>Télécharger en PDF</button>
 
     <div id="cv">
-        
+        <div class="aside">
+            <div class="picture">
+
+            </div>
+        </div>
+
+        <div class="header">
+
+        </div>
+        <div class="body">
+            Hello
+        </div>
     </div>
 </main>
 
 <style>
+    :root {
+        --cv-width: 210mm;
+        --cv-height: 297mm;
+
+        --padding: 8mm;
+ 
+        --aside-width: 63mm;
+        --picture-size: 55mm;
+    }
+
     main {
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
         gap: 20px;
-        margin-block: 50px;
+        margin-block: 30px 50px;
     }
 
     #cv {
-        background-color: red;
+        position: relative;
         width: 210mm;
         height: 297mm;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .aside {
+        background: red;
+        position: absolute;
+        top: var(--padding);
+        left: var(--padding);
+        width: var(--aside-width);
+        height: calc(var(--cv-height) - 2*var(--padding));
+        border-radius: var(--aside-width);
+    }
+
+    .picture {
+        position: absolute;
+        top: calc((var(--aside-width) - var(--picture-size)) / 2);
+        left: calc((var(--aside-width) - var(--picture-size)) / 2);
+        background: #fff;
+        width: var(--picture-size);
+        height: var(--picture-size);
+        border-radius: 100%;
+    }
+
+    .header {
+        background: #000;
+        width: 100%;
+        height: calc((var(--aside-width) / 2) + var(--padding));
+    }
+
+    .body {
+        background: blue;
+        width: 100%;
+        flex: 1;
+        padding-block: var(--padding);
+        padding-inline: calc(2 * var(--padding) + var(--aside-width)) var(--padding);
     }
 </style>
